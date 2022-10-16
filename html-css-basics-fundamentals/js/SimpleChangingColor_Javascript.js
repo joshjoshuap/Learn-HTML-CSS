27066b700b4c97e7e213defc953249d58ffca41d
Joshua <joshjoshuap1@gmail.com>
2022-10-16 17:54:14 +0800

    rename, fix, upload

---
+++ html-css-basics-fundamentals/js/SimpleChangingColor_Javascript.js
@@ -0,0 +1,23 @@
+
+var changeBG = document.querySelector('.ChangingColor'); 
+          
+          
+                   
+function blue() { 
+changeBG.style.background = 'blue'; 
+        
+}   
+
+
+
+function red() { 
+changeBG.style.background = 'red'; 
+        
+}   
+
+
+
+function green() { 
+changeBG.style.background = 'green';  
+        
+}   
